f594c16763df5d0bf7bc6d86b763326c3ed79d7c
snakept <snakept@icloud.com>
2021-04-30 13:33:44 +0200

    created base window and first request to show actor names

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-# Start of project
-print("Hello world!")
+import pandas as pd
+import requests as req
+
+if __name__ == "__main__":
+    actressRequest = req.get(
+        "https://imdb-api.com/en/API/IMDbList/k_vhvsnz5j/ls053501318").content
+    actressData = pd.read_json(actressRequest)
+    actress = actressData['items']
+    for actor in actress:
+        print(actor['fullTitle'])
